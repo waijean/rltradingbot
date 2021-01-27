e0827676565b2e27550e7f2009936532c76710ca
snowman168 <77334975+snowman168@users.noreply.github.com>
2021-01-27 14:22:47 +0000

    technical ind

--- nicky_testing.py
+++ nicky_testing.py
@@ -10,6 +10,11 @@ import os
 from yahoodownloader import YahooDownloader
 import sys
 
+test = np.load('linear_rl_trader_rewards/test.npy')
+import pandas as pd
+test = pd.DataFrame(test)
+
+from stockstats import StockDataFrame as Sdf
 
 gcp_hack = ['aapl', 'msi', 'sbux']
 
@@ -18,3 +23,31 @@ data_df = YahooDownloader(start_date = '2015-01-01',
                           ticker_list = gcp_hack).fetch_data()
 data_df.head()
 
+tech_indicator_list = ['macd', 'rsi_30', 'cci_30', 'dx_30']
+
+def add_technical_indicator(data, tech_indicator_list):
+        """
+        calcualte technical indicators
+        use stockstats package to add technical inidactors
+        :param data: (df) pandas dataframe
+        :return: (df) pandas dataframe
+        """
+        df = data.copy()
+        stock = Sdf.retype(df.copy())
+        unique_ticker = stock.tic.unique()
+
+        for indicator in tech_indicator_list:
+            indicator_df = pd.DataFrame()
+            for i in range(len(unique_ticker)):
+                try:
+                    temp_indicator = stock[stock.tic == unique_ticker[i]][indicator]
+                    temp_indicator = pd.DataFrame(temp_indicator)
+                    indicator_df = indicator_df.append(
+                        temp_indicator, ignore_index=True
+                    )
+                except Exception as e:
+                    print(e)
+            df[indicator] = indicator_df
+        return df
+
+data_t = add_technical_indicator(data_df, tech_indicator_list)
