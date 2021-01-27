6effcf162b3574e602b731d93e5f7ca2341ed72e
taekbokim <51513585+taekbokim@users.noreply.github.com>
2021-01-27 19:19:39 +0000

    Packaged for GCP jobs take6

--- trainer/utils.py
+++ trainer/utils.py
@@ -9,7 +9,7 @@ def get_data():
     # 1 = MSI
     # 2 = SBUX
     # Let's use AAPL (Apple), MSI (Motorola), SBUX (Starbucks)
-    df = pd.read_csv("aapl_msi_sbux.csv")
+    df = pd.read_csv("./aapl_msi_sbux.csv")
     return df.values
 
 
