ac5b61240b6d41b96812de46ef3be24e94008967
taekbokim <51513585+taekbokim@users.noreply.github.com>
2021-01-28 11:20:18 +0000

    Packaged for GCP jobs with HT

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ['numpy','sklearn','datetime','pandas','argparse','matplotlib','gcsfs','hypertune']
+REQUIRED_PACKAGES = ['numpy','sklearn','datetime','pandas','argparse','matplotlib','gcsfs']
 
 setup(
     name='rltradingbot',
